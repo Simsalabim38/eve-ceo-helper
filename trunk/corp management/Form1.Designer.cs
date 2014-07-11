34c1d8b54fd59d97a9e172ef25d04b0c6ebf5ca0
ngb259@gmail.com <ngb259@gmail.com@60136868-43bc-9889-6c80-7877978bb495>
2014-07-11 23:15:14 +0000

    update to api key form

--- trunk/corp management/Form1.Designer.cs
+++ trunk/corp management/Form1.Designer.cs
@@ -28,28 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.SystemColors.Control;
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(33, 47);
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(100, 13);
+            this.textBox1.Size = new System.Drawing.Size(45, 13);
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Key ID:";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(33, 66);
+            this.textBox2.Location = new System.Drawing.Point(118, 38);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(126, 20);
             this.textBox2.TabIndex = 1;
@@ -58,25 +60,25 @@
             // 
             this.textBox3.BackColor = System.Drawing.SystemColors.Control;
             this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Location = new System.Drawing.Point(33, 134);
+            this.textBox3.Location = new System.Drawing.Point(12, 68);
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(100, 13);
+            this.textBox3.Size = new System.Drawing.Size(97, 13);
             this.textBox3.TabIndex = 2;
             this.textBox3.Text = "Verification Code:";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(33, 153);
+            this.textBox4.Location = new System.Drawing.Point(118, 65);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(366, 20);
             this.textBox4.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 268);
+            this.button1.Location = new System.Drawing.Point(440, 102);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(203, 39);
+            this.button1.Size = new System.Drawing.Size(97, 27);
             this.button1.TabIndex = 4;
             this.button1.Text = "Submit";
             this.button1.UseVisualStyleBackColor = true;
@@ -85,26 +87,37 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(33, 196);
+            this.checkBox1.Location = new System.Drawing.Point(281, 108);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(97, 17);
             this.checkBox1.TabIndex = 5;
             this.checkBox1.Text = "Remember API";
             this.checkBox1.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Please enter your Corporation API";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(434, 371);
+            this.ClientSize = new System.Drawing.Size(549, 141);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "API Key";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -118,6 +131,7 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
