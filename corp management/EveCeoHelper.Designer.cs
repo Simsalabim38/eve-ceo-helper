48ca8ed48d02583f0069f36671f6ed1e8da91fe7
trekkie357@gmail.com <trekkie357@gmail.com@60136868-43bc-9889-6c80-7877978bb495>
2014-08-20 04:52:55 +0000

    added a button renamed a teb

--- corp management/EveCeoHelper.Designer.cs
+++ corp management/EveCeoHelper.Designer.cs
@@ -43,7 +43,6 @@
             this.tabControl3 = new System.Windows.Forms.TabControl();
             this.tabPage8 = new System.Windows.Forms.TabPage();
             this.tabPage9 = new System.Windows.Forms.TabPage();
-            this.corpWalletTransactions1 = new corp_management.Controls.CorpWalletTransactions();
             this.tabPage15 = new System.Windows.Forms.TabPage();
             this.TaxContributionTotalText = new System.Windows.Forms.Label();
             this.Label_TotalTaxContribution = new System.Windows.Forms.Label();
@@ -67,7 +66,6 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.tabPage7 = new System.Windows.Forms.TabPage();
-            this.corpPOS1 = new corp_management.Controls.CorpPOS();
             this.tabPage14 = new System.Windows.Forms.TabPage();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -80,6 +78,15 @@
             this.tabPage20 = new System.Windows.Forms.TabPage();
             this.webBrowser2 = new System.Windows.Forms.WebBrowser();
             this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.corpWalletTransactions1 = new corp_management.Controls.CorpWalletTransactions();
+            this.corpPOS1 = new corp_management.Controls.CorpPOS();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -88,6 +95,7 @@
             this.tabPage3.SuspendLayout();
             this.tabPage5.SuspendLayout();
             this.tabControl3.SuspendLayout();
+            this.tabPage8.SuspendLayout();
             this.tabPage9.SuspendLayout();
             this.tabPage15.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -100,6 +108,8 @@
             this.tabControl5.SuspendLayout();
             this.tabPage19.SuspendLayout();
             this.tabPage20.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // tabControl1
@@ -119,6 +129,8 @@
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.button5);
             this.tabPage1.Controls.Add(this.pictureBox1);
             this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
@@ -224,12 +236,17 @@
             // 
             // tabPage8
             // 
+            this.tabPage8.Controls.Add(this.label7);
+            this.tabPage8.Controls.Add(this.label6);
+            this.tabPage8.Controls.Add(this.dataGridView3);
+            this.tabPage8.Controls.Add(this.dataGridView2);
+            this.tabPage8.Controls.Add(this.comboBox2);
             this.tabPage8.Location = new System.Drawing.Point(4, 22);
             this.tabPage8.Name = "tabPage8";
             this.tabPage8.Padding = new System.Windows.Forms.Padding(3);
             this.tabPage8.Size = new System.Drawing.Size(1138, 564);
             this.tabPage8.TabIndex = 0;
-            this.tabPage8.Text = "Master Wallet";
+            this.tabPage8.Text = "Wallet";
             this.tabPage8.UseVisualStyleBackColor = true;
             this.tabPage8.Click += new System.EventHandler(this.tabPage8_Click);
             // 
@@ -243,14 +260,6 @@
             this.tabPage9.TabIndex = 1;
             this.tabPage9.Text = "Journal";
             this.tabPage9.UseVisualStyleBackColor = true;
-            // 
-            // corpWalletTransactions1
-            // 
-            this.corpWalletTransactions1.Corp = null;
-            this.corpWalletTransactions1.Location = new System.Drawing.Point(6, 6);
-            this.corpWalletTransactions1.Name = "corpWalletTransactions1";
-            this.corpWalletTransactions1.Size = new System.Drawing.Size(685, 422);
-            this.corpWalletTransactions1.TabIndex = 0;
             // 
             // tabPage15
             // 
@@ -496,14 +505,6 @@
             this.tabPage7.Text = "POS";
             this.tabPage7.UseVisualStyleBackColor = true;
             // 
-            // corpPOS1
-            // 
-            this.corpPOS1.Corp = null;
-            this.corpPOS1.Location = new System.Drawing.Point(7, 6);
-            this.corpPOS1.Name = "corpPOS1";
-            this.corpPOS1.Size = new System.Drawing.Size(1131, 582);
-            this.corpPOS1.TabIndex = 0;
-            // 
             // tabPage14
             // 
             this.tabPage14.Controls.Add(this.textBox2);
@@ -641,6 +642,86 @@
             this.webBrowser1.Size = new System.Drawing.Size(1140, 589);
             this.webBrowser1.TabIndex = 0;
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(496, 17);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(135, 21);
+            this.comboBox2.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(618, 100);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(514, 450);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(6, 100);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(530, 450);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(839, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 20);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "Transaction";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(183, 77);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 20);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "JOURNAL";
+            // 
+            // corpWalletTransactions1
+            // 
+            this.corpWalletTransactions1.Corp = null;
+            this.corpWalletTransactions1.Location = new System.Drawing.Point(6, 6);
+            this.corpWalletTransactions1.Name = "corpWalletTransactions1";
+            this.corpWalletTransactions1.Size = new System.Drawing.Size(685, 422);
+            this.corpWalletTransactions1.TabIndex = 0;
+            // 
+            // corpPOS1
+            // 
+            this.corpPOS1.Corp = null;
+            this.corpPOS1.Location = new System.Drawing.Point(7, 6);
+            this.corpPOS1.Name = "corpPOS1";
+            this.corpPOS1.POSes = null;
+            this.corpPOS1.Size = new System.Drawing.Size(1131, 582);
+            this.corpPOS1.TabIndex = 0;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(425, 540);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 28);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "load all";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(291, 392);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(400, 128);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = resources.GetString("textBox4.Text");
+            // 
             // EveCeoHelper
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -660,6 +741,8 @@
             this.tabPage3.ResumeLayout(false);
             this.tabPage5.ResumeLayout(false);
             this.tabControl3.ResumeLayout(false);
+            this.tabPage8.ResumeLayout(false);
+            this.tabPage8.PerformLayout();
             this.tabPage9.ResumeLayout(false);
             this.tabPage15.ResumeLayout(false);
             this.tabPage15.PerformLayout();
@@ -675,6 +758,8 @@
             this.tabControl5.ResumeLayout(false);
             this.tabPage19.ResumeLayout(false);
             this.tabPage20.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -691,7 +776,6 @@
         private System.Windows.Forms.TabPage tabPage6;
         private System.Windows.Forms.TabControl tabControl3;
         private System.Windows.Forms.TabPage tabPage8;
-        private System.Windows.Forms.TabPage tabPage9;
         private System.Windows.Forms.TabControl tabControl4;
         private System.Windows.Forms.TabPage tabPage11;
         private System.Windows.Forms.TabPage tabPage12;
@@ -729,7 +813,15 @@
         private System.Windows.Forms.TabPage tabPage20;
         private System.Windows.Forms.WebBrowser webBrowser2;
         private System.Windows.Forms.WebBrowser webBrowser3;
-        private Controls.CorpWalletTransactions corpWalletTransactions1;
         private Controls.CorpPOS corpPOS1;
+        private System.Windows.Forms.TabPage tabPage9;
+        private Controls.CorpWalletTransactions corpWalletTransactions1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button5;
     }
 }
